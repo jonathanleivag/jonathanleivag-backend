e843c8e9c49af24cfe6b7e3a8f4af4c7a9589237
jonathanleivag <email@jonathanleivag.cl>
2022-07-23 21:01:27 -0400

    auth0

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import express from 'express'
+import { auth, claimIncludes } from 'express-oauth2-jwt-bearer'
 import morgan from 'morgan'
 
 import { IDoneMocha } from './interfaces'
@@ -10,6 +11,14 @@ const optionCors: cors.CorsOptions = {
   origin: [],
   credentials: true
 }
+
+const checkJwt = auth({
+  audience: 'http://localhost:3999/',
+  issuerBaseURL: 'https://jonathanleivag.us.auth0.com/',
+  secret:
+    'Consequat duis Lorem eiusmod do ipsum ipsum. Qui aute dolore sit do laboris consectetur incididunt magna nisi ex ex ad. Tempor velit reprehenderit non aliquip reprehenderit adipisicing nulla velit culpa. Ad ex pariatur incididunt ipsum ut incididunt. Irure ex irure non qui.',
+  tokenSigningAlg: 'HS256'
+})
 
 const server = ({ done }: IDoneMocha = { done: undefined }) => {
   const app = express()
@@ -23,6 +32,11 @@ const server = ({ done }: IDoneMocha = { done: undefined }) => {
   app.disable('x-powered-by')
 
   /* ---------------------------------- rutas --------------------------------- */
+
+  app.get('/', checkJwt, claimIncludes('superuser'), (req, res) => {
+    res.send('Hello World!')
+  })
+
   app.use('/api/portafolio', require('./routers/PortafolioRouter'))
   app.use('/api/role', require('./routers/RoleRouter'))
   app.use('/api/user', require('./routers/UserRouter'))
